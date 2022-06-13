e7c932fae2c29179dd7c29911d69687471ff4caf
William Arnold <will748@gmail.com>
2022-06-13 14:43:42 -0700

    Add secp256k1 HPKE

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/bwesterb/go-ristretto v1.2.1
+	github.com/ethereum/go-ethereum v1.10.18 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd
 	golang.org/x/sys v0.0.0-20220315194320-039c03cc5b86
 )
--- hpke/hpke_test.go
+++ hpke/hpke_test.go
@@ -3,7 +3,9 @@ package hpke_test
 import (
 	"bytes"
 	"crypto/rand"
+	"encoding/hex"
 	"fmt"
+	"testing"
 
 	"github.com/cloudflare/circl/hpke"
 )
@@ -60,4 +62,64 @@ func Example() {
 	// Plaintext was sent successfully.
 	fmt.Println(bytes.Equal(ptAlice, ptBob))
 	// Output: true
+}
+
+func TestWorks(t *testing.T) {
+	// import "github.com/cloudflare/circl/hpke"
+	// import "crypto/rand"
+
+	// HPKE suite is a domain parameter.
+	kemID := hpke.KEM_K256_HKDF_SHA256
+	kdfID := hpke.KDF_HKDF_SHA256
+	aeadID := hpke.AEAD_AES128GCM
+	suite := hpke.NewSuite(kemID, kdfID, aeadID)
+	info := []byte("public info string, known to both Alice and Bob")
+
+	// Bob prepares to receive messages and announces his public key.
+	publicBob, privateBob, err := kemID.Scheme().GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	Bob, err := suite.NewReceiver(privateBob, info)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Alice gets Bob's public key.
+	Alice, err := suite.NewSender(publicBob, info)
+	if err != nil {
+		t.Fatal(err)
+	}
+	enc, sealer, err := Alice.Setup(rand.Reader)
+	t.Log("encap", hex.EncodeToString(enc))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Alice encrypts some plaintext and sends the ciphertext to Bob.
+	ptAlice := []byte("text encrypted to Bob's public key")
+	aad := []byte("additional public data")
+	ct, err := sealer.Seal(ptAlice, aad)
+	t.Log("ct", hex.EncodeToString(ct))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Bob decrypts the ciphertext.
+	opener, err := Bob.Setup(enc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ptBob, err := opener.Open(ct, aad)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Plaintext was sent successfully.
+	if !bytes.Equal(ptAlice, ptBob) {
+		t.Fatalf("Plaintext disagrees")
+	}
+	skBytes, _ := privateBob.MarshalBinary()
+	t.Log("sk", hex.EncodeToString(skBytes))
+	t.Fatal()
 }
